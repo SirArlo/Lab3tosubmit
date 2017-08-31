1b313b759bb4242a331af6c09500a7d0880eceaf
Arlo <eardsa@gmail.com>
2017-08-31 16:43:25 +0200

    Exercise 3.1

--- src/smart-pointers/person-main.cpp
+++ src/smart-pointers/person-main.cpp
@@ -20,37 +20,39 @@ Person printName()
 {
 	Person thabo{"Thabo",12};
 	cout << thabo.name() << endl;
-	return thabo;
+	return thabo;   // thabo goes out of scope here after the function printName() is finished
 }
 
 shared_ptr<Person> printName2()
 {
 	auto maryanne_ptr = make_shared<Person>("Maryanne",12);
 	cout << maryanne_ptr->name() << endl;
-	return maryanne_ptr;
+	return maryanne_ptr; // maryanne_ptr goes out of scope here after printName2() is finished and is copied once to person_ptr_in_main
 }
 
 void printName3(shared_ptr<Person> person_ptr)
 {
-	cout << person_ptr->name() << endl;
-	return;
+	cout << person_ptr->name() << endl; 
+	return; //person_ptr will go out of scope here 
 }
 
 
 int main()
 {
-    auto sandile_ptr = make_unique<Person>("Sandile",15);    
+    auto sandile_ptr = make_unique<Person>("Sandile",15);    // sandile_ptr is not copied it is overwritten
 
-    auto thabo_in_main = printName();
+    auto thabo_in_main = printName();        
     cout << thabo_in_main.name() << endl;
 
-    auto person_ptr_in_main = printName2();
-    printName3(person_ptr_in_main);
+    auto person_ptr_in_main = printName2();    
+    printName3(person_ptr_in_main);  //person_ptr_in_main is copied once to the printName3() function
 
-    sandile_ptr = make_unique<Person>("Sandile2",11);  
+    sandile_ptr = make_unique<Person>("Sandile2",11);  // memory for sandile_ptr on line 42 is released here
     cout << sandile_ptr->age() << endl;
 
-    return 0;
+    return 0; // sandile_ptr, thabo_in_main and person_ptr_in_main will go out of scope here
 }
 
+//thabo_in_main will go out of scope first due to dependancy
 
+//all memory for all pointees is released at the end of main
